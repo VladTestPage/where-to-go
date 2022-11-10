868842de7aaed73d1fc7baefd4b5860350a04611
VladTestPage <kissntrill@gmail.com>
2022-11-10 15:39:04 +0300

    add: links add

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,27 @@
 const placesLibrary = [
-  'Charlie',
-  'Darling',
-  'Wine & pinsa',
-  'Maslow6',
-  'Underdog',
-  'Le gusse',
-  'Vershy',
-  'La Scala',
-  'Imena',
-  'Bloom',
-  'Michale',
-  'Ronin',
-  'Marble',
-  'Klaus',
-  'Pinsa Romano',
-  'Flow',
-  'News cafe',
-  'Milano',
+  ['Charlie', 'https://www.instagram.com/charlierestbar/'],
+  ['Darling', 'https://www.instagram.com/darling.minsk/'],
+  ['Wine & pinsa', 'https://www.instagram.com/winepinsa.bar/'],
+  ['Maslow6', 'https://www.instagram.com/maslow6_minsk/'],
+  ['Underdog', 'https://www.instagram.com/cafe5.minsk/'],
+  ['Le gusse', 'https://www.instagram.com/legossecafe/'],
+  ['Vershy', 'https://www.instagram.com/vershy.19/'],
+  ['La Scala', 'https://www.instagram.com/lascalatrattoria/'],
+  ['Imena', 'https://www.instagram.com/imena.minsk/'],
+  ['Bloom', 'https://www.instagram.com/bloomeatery_minsk/'],
+  ['Michale', 'https://www.instagram.com/michelcafe.minsk/'],
+  ['Ronin', 'https://www.instagram.com/ronin.by/'],
+  ['Marble', ''],
+  ['Klaus', 'https://www.instagram.com/klaus.cafe/'],
+  ['Pinsa Romano', 'https://www.instagram.com/pinsa.romana.by/'],
+  ['Flow', 'https://www.instagram.com/flow.izakaybar/'],
+  ['News cafe', 'https://www.instagram.com/news_cafe_minsk/'],
+  ['Milano', 'https://www.instagram.com/milanocafe_minsk/'],
+  ['Fiori', 'https://www.instagram.com/fiori.minsk/'],
 ]
 const button = document.querySelector('.shuffle')
-const display = document.querySelector('.display')
+const displayTitle = document.querySelector('.display__title')
+const displayLink = document.querySelector('.display__link')
 let place = ''
 const randomizer = () => {
   let index = Math.floor(Math.random() * placesLibrary.length)
@@ -29,5 +31,6 @@ const randomizer = () => {
 
 button.addEventListener('click', () => {
   randomizer()
-  display.innerText = place
+  displayTitle.innerText = place[0]
+  displayLink.innerHTML = `<a class="display__link" href="${place[1]}" target="_blank">Instagram</a>`
 })
